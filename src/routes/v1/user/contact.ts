203704c708c76877bb39eab8cbd6a58249300ecd
Oriyomi Adepitan <oadepitan@softalliance.com>
2021-05-21 20:34:31 +0100

    firebase update

--- src/routes/v1/user/contact.ts
+++ src/routes/v1/user/contact.ts
@@ -11,8 +11,8 @@ import role from '../../../helpers/role';
 import admin from '../../../helpers/firebaseAdmin';
 
 const router = express.Router();
-const db = admin.firestore();
-const contact = db.collection('addressBook');
+const db = admin.database();
+const contactRef = db.ref('addressBook');
 
 /*-------------------------------------------------------------------------*/
 // Below all APIs are private APIs protected for writer's role
@@ -24,8 +24,8 @@ router.post(
   validator(schema.contactCreate),
   asyncHandler(async (req: ProtectedRequest, res) => {
     const { firstName, lastName, address, phoneNumber } = req.body;
-    let docRef = contact.doc(firstName);
-    await docRef.set({
+    const userContactId = contactRef.push().key;
+    await contactRef.child(userContactId!).set({
       lastName,
       phoneNumber,
       address,
