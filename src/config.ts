af894a70be9e9c55e5aa6a857e3a09089b782633
Oriyomi Adepitan <oadepitan@softalliance.com>
2021-05-21 07:00:18 +0100

    prot binding

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Mapper for environment variables
 export const environment = process.env.NODE_ENV;
-export const port = process.env.PORT || 5000;
+export const port = (process.env.PORT as unknown as number) || 3000;
 
 export const db = {
   name: process.env.DB_NAME || '',
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { port } from './config';
 import app from './app';
 
 app
-  .listen(port, () => {
+  .listen(port, '0.0.0.0', () => {
     Logger.info(`server running on port : ${port}`);
   })
   .on('error', (e) => Logger.error(e));
