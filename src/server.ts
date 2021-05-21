bdd82be468564feb55756c5659a251b70446cf85
Oriyomi Adepitan <oadepitan@softalliance.com>
2021-05-21 05:02:30 +0100

    port binding

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import Logger from './core/Logger';
 import { port } from './config';
 import app from './app';
 
+console.log(port);
+
 app
   .listen(port, () => {
     Logger.info(`server running on port : ${port}`);
